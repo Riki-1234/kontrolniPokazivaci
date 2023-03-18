334fecead190fd58b4942509f14a0cd5d80ad603
riki-1234 <49783868+Riki-1234@users.noreply.github.com>
2023-03-18 18:18:40 +0100

    Initial Commit

---
+++ najmanjiBroj.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Unesite tri broja: ";
+	cin >> a >> b >> c;
+
+	int min;
+	int* a_ptr = &a, * b_ptr = &b, * c_ptr = &c;
+	if (*a_ptr < *b_ptr && *a_ptr < *c_ptr) {
+		min = *a_ptr;
+	}
+	if (*b_ptr < *a_ptr && *b_ptr < *c_ptr) {
+		min = *b_ptr;
+	}
+	if (*c_ptr < *b_ptr && *c_ptr < *a_ptr) {
+		min = *c_ptr;
+	}
+	cout << "Najmanji broj je: " << min;
+}
